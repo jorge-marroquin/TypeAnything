9d087fef5024e56fadda9e89e7ff9f01fdaf3774
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 17:24:45 -0600

    Create libros.js

---
+++ libros.js
@@ -0,0 +1,18 @@
+const libros=[
+{
+titulo:"El Principito",
+autor:"Antoine de Saint-Exupéry",
+texto:`
+Cuando yo tenía seis años...
+`
+},
+
+{
+titulo:"Otro libro",
+autor:"Autor",
+texto:`
+Texto del libro...
+`
+}
+
+];
