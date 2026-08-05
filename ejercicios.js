b188ece767b694387bae27d8d3def0f44d3b438c
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 17:25:28 -0600

    Create ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,24 @@
+const ejercicios=[
+{
+nombre:"Terminación CIÓN",
+texto:`
+acción información comunicación educación organización
+`
+},
+
+{
+nombre:"Palabras con NTE",
+texto:`
+mente puente presente constante diferente importante
+`
+},
+
+{
+nombre:"Sílabas difíciles",
+texto:`
+tra tre tri tro tru
+cla cle cli clo clu
+`
+}
+
+];
