2e61149436d7b5b615c23b7c4b6cd26cb9f7ada4
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 19:09:50 -0600

    Update libros.js

--- js/libros.js
+++ js/libros.js
@@ -1,18 +1,29 @@
-const libros=[
-{
-titulo:"El Principito",
-autor:"Antoine de Saint-Exupéry",
-texto:`
-Cuando yo tenía seis años...
-`
-},
+<div id="lista-libros"></div>
 
-{
-titulo:"Otro libro",
-autor:"Autor",
-texto:`
-Texto del libro...
-`
-}
+<script src="js/libros.js"></script>
 
-];
+<script>
+
+const lista = document.getElementById("lista-libros");
+
+
+libros.forEach(libro=>{
+
+    let boton=document.createElement("button");
+
+    boton.textContent="📖 "+libro.titulo;
+
+
+    boton.onclick=()=>{
+
+        window.location.href=
+        "capitulos.html?libro="+libro.id;
+
+    };
+
+
+    lista.appendChild(boton);
+
+});
+
+</script>
