18553f1796acbf932d04e192e89bdfe344c96f64
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 22:03:31 -0600

    Update libros.js

--- js/libros.js
+++ js/libros.js
@@ -37,5 +37,26 @@ const libros = [
         ]
     }
 
+        {
+        id: "sentir",
+        titulo: "Sentir es el Secreto",
+        autor: "Neville Goddard",
+
+        capitulos: [
+            {
+                nombre: "Introducción",
+                texto: "Texto de introducción..."
+            },
+            {
+                nombre: "Capítulo 1",
+                texto: "Texto del capítulo 1..."
+            },
+            {
+                nombre: "Capítulo 2",
+                texto: "Texto del capítulo 2..."
+            }
+        ]
+    }
+
     
 ];
