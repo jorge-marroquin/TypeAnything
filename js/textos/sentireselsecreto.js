cfb9f0b6cabfa24838fc083d4ee71f2411f99b79
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-06 00:36:51 -0600

    Update sentireselsecreto.js

--- js/textos/sentireselsecreto.js
+++ js/textos/sentireselsecreto.js
@@ -10,8 +10,7 @@
 este libro trata del arte de realizar tu deseo. Te da cuenta del mecanismo usado en la producción del mundo visible. Es un libro pequeño pero no leve. Hay un tesoro en él, un camino claro y definido para realizar tus sueños.
 De ser posible llevar convicción a otro por medio de argumentos razonados y ejemplos detallados, este libro sería muchas veces su tamaño. Pocas veces es posible, sin embargo, hacer eso por medio de declaraciones escritas o argumentos dado el juicio suspendido, siempre parece plausible decir que el autor fue deshonesto o ingenuo, y, por lo tanto, su evidencia estuvo contaminada. Consecuentemente, he omitido intencionalmente todos los argumentos y testimonios, y simplemente reto al lector de mente abierta a practicar la ley de la conciencia como se revela en este libro. El éxito personal será una prueba mucho más convincente que todos los libros que pudieran ser escritos respecto al tema.
 
-NEVILLE
-"
+NEVILLE"
             },
             {
                 nombre: "Capítulo 1",
