dad5582874d0ef1cf34dde1177248130b0031ea6
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 22:35:15 -0600

    Update tema.js

--- js/tema.js
+++ js/tema.js
@@ -1,19 +1,25 @@
-// Restaurar tema
-if(localStorage.getItem("tema") === "oscuro"){
-    document.body.classList.add("modo-oscuro");
-    bt.textContent = "☀️ Modo Claro";
-}
+const bt = document.getElementById("boton-tema");
 
-bt.onclick = ()=>{
+if(bt){
 
-    document.body.classList.toggle("modo-oscuro");
-
-    if(document.body.classList.contains("modo-oscuro")){
-        localStorage.setItem("tema","oscuro");
+    // Restaurar tema
+    if(localStorage.getItem("tema") === "oscuro"){
+        document.body.classList.add("modo-oscuro");
         bt.textContent = "☀️ Modo Claro";
-    }else{
-        localStorage.setItem("tema","claro");
-        bt.textContent = "🌙 Modo Nocturno";
     }
 
-};
+    bt.onclick = ()=>{
+
+        document.body.classList.toggle("modo-oscuro");
+
+        if(document.body.classList.contains("modo-oscuro")){
+            localStorage.setItem("tema","oscuro");
+            bt.textContent = "☀️ Modo Claro";
+        }else{
+            localStorage.setItem("tema","claro");
+            bt.textContent = "🌙 Modo Nocturno";
+        }
+
+    };
+
+}
