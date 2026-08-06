1947b26ad756ca66c562e4401706d52505d72c20
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-05 22:56:08 -0600

    Create SentireselSecreto.js

---
+++ textos/libros/SentireselSecreto.js
@@ -0,0 +1,20 @@
+    {
+        id: "SentireselSecreto",
+        titulo: "Sentir es el Secreto",
+        autor: "Neville Goddard",
+
+        capitulos: [
+            {
+                nombre: "Introducción",
+                texto: "Texto de introducción..."
+            },
+            {
+                nombre: "Capítulo 1",
+                texto: "Texto del capítulo 1..."
+            },
+            {
+                nombre: "Capítulo 2",
+                texto: "Texto del capítulo 2..."
+            }
+        ]
+    }
