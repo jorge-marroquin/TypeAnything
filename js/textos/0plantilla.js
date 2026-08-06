f3b0486e95e93a30a5cb50f6534bd7f2624cbeaf
jorge-marroquin <jorgemarroquin2410@gmail.com>
2026-08-06 00:51:54 -0600

    Update 0plantilla.js

--- js/textos/0plantilla.js
+++ js/textos/0plantilla.js
@@ -1,1 +1,18 @@
+        const libro = {
+        id: "libro",
+        titulo: "Libro",
+        autor: "Autor",
+
+        capitulos: [
+
+            {
+                nombre: "Capítulo 1",
+                texto: `Texto del capítulo 1`
+            },
+            {
+                nombre: "Capítulo 2",
+                texto: `Texto del capítulo 2...`
+            }
+        ]
+    }
 
